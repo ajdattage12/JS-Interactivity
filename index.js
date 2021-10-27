a81123be1e8b1bb81100e8900e029600960f6bce
Ariell Dattage <ariell.dattage@Ariells-MBP.localdomain>
2021-10-27 13:47:47 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//console.log("Hello World")
+
+
+let inputField = document.querySelector('#inputField')
+
+const addMovie = (event) =>{
+    return event 
+}
+let movie = document.createElement()
+let movieTitle = inputField.textContent
