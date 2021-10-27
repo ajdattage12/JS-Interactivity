ea2bdcc44aae46c936a3b3abc9f4138e2472da60
Ariell Dattage <ariell.dattage@Ariells-MBP.localdomain>
2021-10-27 15:34:57 -0600

    finished first section

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 //console.log("Hello World")
+let message = document.querySelector('#message')
 
-
-let inputField = document.querySelector('#inputField')
+let inputField = document.querySelector('input')
+let addButton = document.querySelector('button')
 
 const addMovie = (event) =>{
-    return event 
+    event.preventDefault()
+    let movie = document.createElement("li")
+    let movieTitle = document.createElement("span")
+    movieTitle.textContent = inputField.value
+    movieTitle.addEventListener('click', crossedOffMovie)
+    movie.appendChild(movieTitle)
+    
+    deleteButton = document.createElement('button')
+    deleteButton.textContent = "X"
+    deleteButton.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteButton)
+
+    let list = document.querySelector('ul')
+    list.appendChild(movie)
+
+    inputField.value = ""
 }
-let movie = document.createElement()
-let movieTitle = inputField.textContent
+
+const deleteMovie = (event) => {
+    event.target.parentNode.remove()
+    message.textContent = "Movie Deleted!"
+}
+const crossedOffMovie = (event) =>{
+    event.target.classList.toggle('checked')
+    if(event.target.classList = event.target.classList.contains ('checked')){
+        message.textContent = "Moive Watched!"
+    } else {
+        message.textContent = "Movie added back!"
+    }
+}
+
+
+// let form = document.querySelector('form')
+addButton.addEventListener('click', addMovie)
